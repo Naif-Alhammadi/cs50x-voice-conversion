a086974758678b44f6fc84a48011bd5dd13852f5
Naif-Alhammadi <naifalhammadi2@gmail.com>
2026-08-25 02:35:22 +0300

    feat: add initial session and login API

--- app.py
+++ app.py
@@ -1,8 +1,21 @@
-from flask import Flask, render_template,request, redirect, session
+from flask import Flask, render_template,request, redirect, session, jsonify
 
+from flask_session import Session
 app = Flask(__name__)
 
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/login", methods=["POST"])
+def login():
+    user_data = request.get_json()
+    print(user_data)
+    if not user_data["name"] or not user_data["email"] or not user_data["password"] or not user_data["verifypasswrod"]:
+        return jsonify("please fill all fileds")
+
+    return jsonify("noerror")
--- static/scripts/form.js
+++ static/scripts/form.js
@@ -6,6 +6,14 @@ const donotHave = document.querySelector("#donotHave");
 const register = document.querySelector(".register");
 const login = document.querySelector(".login");
 
+const loginForm = document.querySelector("#login-form");
+
+const loginName = document.querySelector("#name");
+const loginPassword = document.querySelector("#password");
+const loginVerifyPassword = document.querySelector("#verifypassword");
+const loginEmail = document.querySelector("#email");
+
+
 haveAnAccount.addEventListener("click", function(){
     login.style.transform = "translateX(-260px)";
     register.style.transform = "translateX(0px)";
@@ -14,4 +22,29 @@ haveAnAccount.addEventListener("click", function(){
 donotHave.addEventListener("click", function() {
     login.style.transform = "translateX(0px)";
     register.style.transform = "translateX(260px)";
-})
+});
+
+
+loginForm.addEventListener("submit", event => {
+    event.preventDefault();
+    console.log(loginName.value);
+
+    
+    fetch("/login", {
+    method:"POST",
+    headers: {
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+        "name": loginName.value,
+        "email": loginEmail.value,
+        password: loginPassword.value,
+        "verifypasswrod": loginVerifyPassword.value
+    }),
+}).then(response => response.json()).then(data => {
+    console.log(data);
+}).catch (error => {
+    console.error(error);
+});
+});
+
