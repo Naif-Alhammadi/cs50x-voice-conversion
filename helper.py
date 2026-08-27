d4fcd4fcb462338177d71c5d7166689b1cfa28b6
Naif-Alhammadi <naifalhammadi2@gmail.com>
2026-08-28 00:42:36 +0300

    feat: implement file class with a fileopen method that reads the RIFF header

--- helper.py
+++ helper.py
@@ -12,3 +12,57 @@ def login_required(f):
 
     return decorate
 
+
+class File:
+    def __init__(self, path):
+        self.path = path
+        self.fileFormat = True
+        self.isRIFF = False
+        self.isWAVE = False
+        self.subChunkSize = 0
+        self.audioFormat = 'PCM'
+        self.numChannels = 'stereo'
+        self.sampleRate = 0
+        self.file_open()
+
+    def file_open(self):
+        with open(self.path, 'rb') as file:
+
+            # read file if it contain RIFF and WAVE signature
+
+            # read by 4 byte
+            chunkSize = 4
+
+            while True:
+
+                file_chunk = file.read(chunkSize)
+
+                # if first 4 byte is RIFF IMB stander format
+                if (file_chunk == b'RIFF'):
+                    self.isRIFF = True
+                    continue
+
+                # second 4 byte is WAVE standard .wav files
+                if(file_chunk == b'WAVE'):
+                    self.isWAVE = True
+                    continue
+
+                if(file_chunk == b'fmt '):
+                    chunkSize = chunkSize
+                    self.subChunkSize = file.read(chunkSize)
+                    self.audioFormat = file.read(chunkSize - 2)
+                    if self.audioFormat == b'\x01\x00':
+                        self.audioFormat = 'PCM'
+
+                    stereo = file.read(chunkSize - 2)
+                    if stereo == b'\x02\x00':
+                        pass
+
+                    file.read(chunkSize)
+                    break
+
+
+                self.fileFormat = False
+
+if __name__ == "__main__":
+    f = File("uploads/audios/lack in.wav")
